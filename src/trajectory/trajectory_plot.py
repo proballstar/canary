562969671d795c4324609f67ae95cf94abcc32f6
aaronhma <aaronhma@gmail.com>
2020-04-09 13:58:36 -0700

    Create trajectory_plot.py

---
+++ src/trajectory/trajectory_plot.py
@@ -0,0 +1,14 @@
+# FOR USE IN FIREBOLT LABS:
+# %matplotlib inline 
+# %config InlineBackend.figure_format = 'retina'
+import numpy as np 
+import math
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import jdc
+
+# FOR USE IN FIREBOLT LAS:
+# from test import test_plot
+
+# FOR USE IN FIREBOLT LABS:
+# pylab.rcParams['figure.figsize'] = 10, 10
