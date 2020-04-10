ddb502d4e79c9e6c682efa2f8c85af98a909954f
Aaron Ma <aaronhma@gmail.com>
2020-04-09 19:18:30 -0700

    UDPATE project

--- src/external/external.py
+++ src/external/external.py
@@ -1,12 +1,34 @@
 """
 This file defines the drone's external error system in a with-feedback loop.
 """
-from .common import log_states as logger
+from common import log_states as logger
+# from ____ import drone
 noise = False
 external = False
 model = False
 
+logger.__init__()
+logger.WARNING("This API will be deprecated in a future version.")
+
 if bool(model) != False:
-    raise Exception("Model Error")
+    logger.FATAL("Model error detected! Sending signal to drone...")
+    # drone.cmd_position(drone.global_home[0], drone.global_home[1], drone.global_home[2])
+    # Returns to global home x, y, and z
 else:
-    print()
+    logger.SUCCESS("No model error detected!")
+
+if bool(noise) != False:
+    # TODO: Allow user to disable this:
+    # @firebolt-ai Should we?
+    logger.FATAL("A faulty sensor was detected on your drone! Sending signal to drone...")
+    # drone.cmd_position(drone.global_home[0], drone.global_home[1], drone.global_home[2])
+    # Returns to global home x, y, and z
+else:
+    logger.SUCCESS("No model error detected!")
+
+if bool(external) != False:
+    logger.FATAL("An external error was detected! Sending signal to drone...")
+    # drone.cmd_position(drone.global_home[0], drone.global_home[1], drone.global_home[2])
+    # Returns to global home x, y, and z
+else:
+    logger.SUCCESS("No external error detected!")
--- tools/spec/print_results.py
+++ tools/spec/print_results.py
@@ -2,19 +2,19 @@ def INFO(*args):
     return "[INFO] " + args
 
 def WARNING(*args):
-    print("\033[1;33;40m [WARN]  {}".format(*args))
+    print("\033[1;33;40m [WARN]  {} \n".format(*args))
     # raise Warning("[WARN] ", text)
 
 def ERROR(*args):
-    print("\033[1;31;40m [FATAL]  {}".format(*args))
+    print("\033[1;31;40m [FATAL]  {} \n".format(*args))
     # raise Exception("[ERROR] ", text)
 
 def FATAL(*args):
-    print("\033[1;31;40m [FATAL]  {}".format(*args))
+    print("\033[1;31;40m [FATAL]  {} \n".format(*args))
     # raise Exception("[FATAL] ", text)
 
 def SUCCESS(*args):
-    print("\033[1;32;40m [SUCCESS]  {}".format(*args))
+    print("\033[1;32;40m [SUCCESS]  {} \n".format(*args))
 
 def __init__():
     SUCCESS("Logger imported successfully!")
