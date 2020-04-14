9ab6ae46223cae2d86a316b436fa0f293ca2fd2b
proballstar <59456400+proballstar@users.noreply.github.com>
2020-04-13 17:49:12 -0700

    Controller.py

---
+++ src/model/controller.py
@@ -0,0 +1,14 @@
+// The Model
+
+//Same Buttons replace with basic buttons on car
+
+//Using XBox Controlls
+
+def a():
+  #when press a
+def b():
+  #when press b
+def x():
+  #when press x
+def y():
+  #when press y
